b18abbb939559df39da28b321aef754b3f6d6f98
Frederik <s204213@win.dtu.dk>
2024-05-02 13:19:07 +0200

    nbckhjgvljk

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 
-def get_data(hierar = False):
+def get_data(hierar = False, oneHot = False):
 
     #Gets path to folder
     path = os.getcwd()
@@ -24,31 +24,57 @@ def get_data(hierar = False):
     else: 
         age = []
 
-    # One hot encode data: 
-    OHE_train = pd.get_dummies(df_train,columns=col)
-    OHE_test = pd.get_dummies(df_test,columns=col)
+    if oneHot:
+        # One hot encode data: 
+        X_train = pd.get_dummies(df_train,columns=col)
+        X_test = pd.get_dummies(df_test,columns=col)
 
-    #Extract y values:
-    y_train = OHE_train['target'].values.astype("int")
-    y_test = OHE_test['target'].values.astype("int")
+        #Extract y values:
+        y_train = X_train['target'].values.astype("int")
+        y_test = X_test['target'].values.astype("int")
 
-    #Extract data 
-    X_train = OHE_train.loc[:, OHE_train.columns != 'target'].values
-    X_test = OHE_test.loc[:, OHE_test.columns != 'target'].values
+        #Extract data 
+        X_train = X_train.loc[:, X_train.columns != 'target'].values
+        X_test = X_test.loc[:, X_test.columns != 'target'].values
 
-    #Normalize non one-hot-encoded data (training data)
-    X_train_temp = X_train[:,0:5].astype('float')
-    X_mean = X_train_temp.mean(axis=0)
-    X_std = X_train_temp.std(axis=0)
-    X_train_temp = (X_train_temp - X_mean) / X_std
-    X_train[:,0:5] = X_train_temp
+        #Normalize non one-hot-encoded data (training data)
+        X_train_temp = X_train[:,0:5].astype('float')
+        X_mean = X_train_temp.mean(axis=0)
+        X_std = X_train_temp.std(axis=0)
+        X_train_temp = (X_train_temp - X_mean) / X_std
+        X_train[:,0:5] = X_train_temp
 
-    #Normalize non one-hot-encoded data (training data)
-    X_train_temp = X_test[:,0:5].astype('float')
-    X_mean = X_train_temp.mean(axis=0)
-    X_std = X_train_temp.std(axis=0)
-    X_train_temp = (X_train_temp - X_mean) / X_std
-    X_test[:,0:5] = X_train_temp
+        #Normalize non one-hot-encoded data (training data)
+        X_train_temp = X_test[:,0:5].astype('float')
+        X_mean = X_train_temp.mean(axis=0)
+        X_std = X_train_temp.std(axis=0)
+        X_train_temp = (X_train_temp - X_mean) / X_std
+        X_test[:,0:5] = X_train_temp
+    else:
+        X_train = df_train
+        X_test = df_test
+
+        #Extract y values:
+        y_train = X_train['target'].values.astype("int")
+        y_test = X_test['target'].values.astype("int")
+
+        #Extract data 
+        X_train = X_train.loc[:, X_train.columns != 'target'].values
+        X_test = X_test.loc[:, X_test.columns != 'target'].values
+
+        #Normalize non one-hot-encoded data (training data)
+        X_train_temp = X_train.astype('float')
+        X_mean = X_train_temp.mean(axis=0)
+        X_std = X_train_temp.std(axis=0)
+        X_train_temp = (X_train_temp - X_mean) / X_std
+        X_train = X_train_temp
+
+        #Normalize non one-hot-encoded data (training data)
+        X_train_temp = X_test.astype('float')
+        X_mean = X_train_temp.mean(axis=0)
+        X_std = X_train_temp.std(axis=0)
+        X_train_temp = (X_train_temp - X_mean) / X_std
+        X_test = X_train_temp
   
 
     return [X_train,y_train,X_test,y_test,age]
